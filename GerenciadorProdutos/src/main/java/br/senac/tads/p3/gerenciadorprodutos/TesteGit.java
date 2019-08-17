30acb43147ade49aba13608615fdb40e3ea8dd0a
dmansoldo@gmail.com <diego.amalmeida@CAS555488W1030.senacsp.edu.br>
2019-08-16 21:36:57 -0300

    Versão Inicial

---
+++ GerenciadorProdutos/src/main/java/br/senac/tads/p3/gerenciadorprodutos/TesteGit.java
@@ -0,0 +1,11 @@
+package br.senac.tads.p3.gerenciadorprodutos;
+
+// @author diego.amalmeida
+public class TesteGit {
+
+    public static void main(String[] args) {
+
+        System.out.println("     TESTES GIT     ");
+
+    }
+}
